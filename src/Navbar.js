95af1d74bfbbe42179534795ab7cc144ecdb73fc
sunkam96 <sunkam.sai@gmail.com>
2023-06-07 14:15:39 -0400

    page scaffolding

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import './App.css'
+
+function Navbar() {
+    return(
+        <div class="navbar">
+            <h3>RateQuote</h3>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/PageContent.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function PageContent(){
+    return (
+        <div className="page-content">
+            <h3>See rates by lender</h3>
+        </div>
+    )
+}
+
+export default PageContent;
